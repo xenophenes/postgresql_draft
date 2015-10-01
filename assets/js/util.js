0462dc2ca88fc3630d0d509aecad09f214ed407e
xenophenes <sarahwiz210@yahoo.com>
2015-10-01 14:19:28 -0700

    Third attempt at quotes module, upload for testing

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,12 +1,3 @@
-// When the DOM is ready, run this function
-$(document).ready(function() {
-	//Set the carousel options
-	$('#quote-carousel').carousel({
-		pause: true,
-		interval: 4000,
-	});
-});
-
 (function($) {
 
 	/**
