ec547375e1485b8e72fe37f44805f6f5bb1fdb94
xenophenes <sarahwiz210@yahoo.com>
2015-10-01 14:15:24 -0700

    Second attempt at quotes module, upload for testing

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,3 +1,12 @@
+// When the DOM is ready, run this function
+$(document).ready(function() {
+	//Set the carousel options
+	$('#quote-carousel').carousel({
+		pause: true,
+		interval: 4000,
+	});
+});
+
 (function($) {
 
 	/**
